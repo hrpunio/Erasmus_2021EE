8e9b02426cebaaa1a3f1314a5ddb7b0ca4378222
tomasz przechlewski <rudolf.von.ems@gmail.com>
2022-01-30 11:01:51 +0100

    aktualizacja

--- wb1.R
+++ wb1.R
@@ -1,3 +1,4 @@
+### Read the data
 wb <- read.csv("WBdata.csv", sep = ';',  header=T, na.string="NA")
 
 library("tidyverse")
@@ -5,10 +6,13 @@ library("tidyverse")
 
 unwated <- c('OSS', 'LTE', 'AFW')
 
-wbl <- wb %>% pivot_wider(names_from = indicatorcode, values_from = value) %>%  
-  filter (year == 2015) %>%
-  select (code, countryname, gdpppp=NY.GDP.PCAP.PP.CD, gdp=NY.GDP.PCAP.CD, co2=EN.ATM.CO2E.PC) %>%
-  filter (! code %in% unwated)
+
+
+wbl <- wb %>% pivot_wider(names_from = indicatorcode, values_from = value) %>%
+  filter (year == 2015) 
+  select (code, countryname, NY.GDP.PCAP.PP.CD, gdp=NY.GDP.PCAP.CD, co2=EN.ATM.CO2E.PC) %>%
+  
+    filter (! code %in% unwated)
 
 plot(wbl$gdp, wbl$co2)
 
