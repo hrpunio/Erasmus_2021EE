93dc2204659c3496a15d0db911a5d368d3a8cd1b
tomasz przechlewski <rudolf.von.ems@gmail.com>
2022-02-05 08:18:15 +0100

    aktualizacja

--- wb1.R
+++ wb1.R
@@ -1,27 +1,141 @@
-### Read the data
+##
+library("tidyverse")
+library("ggplot2")
+library("ggfortify")
+library("forecast")
+library("knitr")
+library("lmtest")
+library("car") # D-W test
+
+###
+
+### Read all the data
+##'NY.GDP.MKTP.KD',    = GDP constant US$ 2015
+##'SP.POP.TOTL',       = Population total
+##'EN.ATM.CO2E.PC',    = CO2 emission in tons per/capita
+##"EG.ELC.RNWX.ZS",    =  Electricity from renovable sources % of total
+##'NY.GDP.PCAP.CD',    = GDP per capita (current US$) 
+##"NY.GDP.PCAP.PP.CD", = GDP per capita, PPP (current international $) 
+##'EG.ELC.COAL.ZS',    = Electricity produced from coal % of total
+##'AG.PRD.LVSK.XD',    = Livstock production index (2014--2016=100)
+##"AG.CON.FERT.ZS",    = Fertilizer consumption (kg/hectare of arable land)
+##"EG.USE.PCAP.KG.OE"  = Energy use (kg of oil equivalent per capita)
+
 wb <- read.csv("WBdata.csv", sep = ';',  header=T, na.string="NA")
 
-library("tidyverse")
+
+### Clean the data
+##unwated <- c('OSS', 'LTE', 'AFW', 'LCN', 'LCD')
+wb.unwanted <- read.csv("wb_groups_list.csv", 
+    sep = ';',  header=T, na.string="NA")
+
+
+## Transform to _wider_ format
+## change colum names to something less complicated
+## Crossectional data: year == 2015
+## Remove small countries: year >= 1990
+wbl <- wb %>% filter (! code %in% wb.unwanted$code ) %>%
+  pivot_wider( names_from = indicatorcode, values_from = value) %>%
+  select (countryname, code, year, 
+          co2=EN.ATM.CO2E.PC, 
+          gdp=NY.GDP.PCAP.PP.CD, 
+          e4coal=EG.ELC.COAL.ZS,
+          fcons=AG.CON.FERT.ZS, 
+          euse=EG.USE.PCAP.KG.OE, 
+          gdpt=NY.GDP.MKTP.KD,  
+          pop=SP.POP.TOTL) %>%
+  filter (year == 2015) %>%
+  filter  (pop > 1000000)
+
+## check sample sime:
+nrow(wbl)
+
+## scatte-plot matrix for co2, gdp, fcons, euse
+wbl %>% select (co2, gdp, fcons, euse, e4coal) %>% pairs()
+
+## correlation matrix for
+## NOTE: na.omit function omits rows with NA values
+wb.corr <- wbl %>% select (co2, gdp, fcons, euse, e4coal) %>% 
+  na.omit() %>% cor()
+wb.corr
 ##
 
-unwated <- c('OSS', 'LTE', 'AFW')
+
+## Model 0
+## $CO2 = a + b GDP$
+
+lm0 <- lm(co2 ~ gdp, data=wbl)
+summary(lm0)
 
 
+## Model 1
+## $\ln CO2 = a + b \ln GDP$
 
-wbl <- wb %>% pivot_wider(names_from = indicatorcode, values_from = value) %>%
-  filter (year == 2015) 
-  select (code, countryname, NY.GDP.PCAP.PP.CD, gdp=NY.GDP.PCAP.CD, co2=EN.ATM.CO2E.PC) %>%
-  
-    filter (! code %in% unwated)
+lm1 <- lm(log(co2) ~ log(gdp), data=wbl)
+summary(lm1)
 
-plot(wbl$gdp, wbl$co2)
+## Model 2
+## $\ln CO2 = a + b \ln GDP $
+lm2 <- lm(log(co2) ~ log(gdp) + I(log(gdp)^2 ), data=wbl)
+summary(lm2)
 
-model1 <- lm( co2 ~ gdp, data = wbl)
+## Perhaps some other models ...
 
-summary(model1)
+## For each model perform diagnostics
 
-plot(wbl$gdpppp, wbl$co2)
+## chart residuals vs fitted
+plot(lm2, which = 1) 
 
+## Check heteroscedasticity
+## Breusch-Pagan Test For Homoscedasticity
+## high p = no heteroscedasticity
+bptest(lm2)
 
-model2 <- lm( co2 ~ gdpppp, data = wbl)
-summary(model2)
+## Check autocorrelation of error term
+## Durbin Watson Test for Autocorrelation
+## high p = errors not autocorrelated
+durbinWatsonTest(model1)
+
+## Normality of residuals
+## visually looking at qq-plot
+plot(lm2, which = 2) 
+qqPlot(lm2)
+
+shapiro.test(lm2$residuals)
+
+## Check multicollinearity
+## for models with 
+vif(lm2)
+
+## lm2 is highly collinear but it is OK
+
+## Select between nested models with anova
+## (significance of difference of variance explainded)
+anova(lm1, lm2, test="F") 
+
+# Model 2 is significantly better than model 1
+
+## Repeat for other sample
+
+wb.highmid <- read.csv("wb_groups.csv", 
+                        sep = ';',  header=T, na.string="NA") %>%
+  ## about 30 countries:
+  ##filter ( GroupName == 'Low income')
+  ##filter ( GroupName == 'High income')
+  filter ( GroupName == 'High income' | GroupName == 'Middle income')
+
+nrow(wb.highmid)
+
+wbl <- wb %>% filter (code %in% wb.highmid$code ) %>%
+  pivot_wider( names_from = indicatorcode, values_from = value) %>%
+  select (countryname, code, year, 
+          co2=EN.ATM.CO2E.PC, 
+          gdp=NY.GDP.PCAP.PP.CD, 
+          e4coal=EG.ELC.COAL.ZS,
+          fcons=AG.CON.FERT.ZS, 
+          euse=EG.USE.PCAP.KG.OE, 
+          gdpt=NY.GDP.MKTP.KD,  
+          pop=SP.POP.TOTL) %>%
+  filter (year == 2015)
+nrow(wbl)
+
